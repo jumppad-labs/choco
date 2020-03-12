6dcef9f19734b7bb97f8157aae24c433ca49a537
Nic Jackson <jackson.nic@gmail.com>
2020-03-12 12:48:30 +0000

    Updated to complete comments from choco maintainer

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,21 +1,7 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
-
-# 1. See the _TODO.md that is generated top level and read through that
-# 2. Follow the documentation below to learn how to create a package for the package type you are creating.
-# 3. In Chocolatey scripts, ALWAYS use absolute paths - $toolsDir gets you to the package's tools directory.
-$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-# Internal packages (organizations) or software that has redistribution rights (community repo)
-# - Use `Install-ChocolateyInstallPackage` instead of `Install-ChocolateyPackage`
-#   and put the binaries directly into the tools folder (we call it embedding)
-#$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
-# If embedding binaries increase total nupkg size to over 1GB, use share location or download from urls
-#$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
-# Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
-# Internal/Organization: Download from internal location (internet sources are unreliable)
 $url = 'https://github.com/shipyard-run/shipyard/releases/download/v__version__/shipyard-windows-amd64.zip' # download url, HTTPS preferred
+$checkSum = "__checksum__"
 
-Install-ChocolateyZipPackage -PackageName 'shipyard' -Url $url -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage -PackageName 'shipyard' -Url $url -UnzipLocation $toolsDir -ChecksumType "sha256" -Checksum $checkSum
 Install-BinFile -Name shipyard -Path "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\shipyard"
--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -1,21 +1,3 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-## NOTE: In 80-90% of the cases (95% with licensed versions due to Package Synchronizer and other enhancements),
-## AutoUninstaller should be able to detect and handle registry uninstalls without a chocolateyUninstall.ps1.
-## See https://chocolatey.org/docs/commands-uninstall
-## and https://chocolatey.org/docs/helpers-uninstall-chocolatey-package
-
-## If this is an MSI, ensure 'softwareName' is appropriate, then clean up comments and you are done.
-## If this is an exe, change fileType, silentArgs, and validExitCodes
-
-$ErrorActionPreference = 'Stop'; # stop on all errors
-
-## OTHER POWERSHELL FUNCTIONS
-## https://chocolatey.org/docs/helpers-reference
-#Uninstall-ChocolateyZipPackage $packageName # Only necessary if you did not unpack to package directory - see https://chocolatey.org/docs/helpers-uninstall-chocolatey-zip-package
-#Uninstall-ChocolateyEnvironmentVariable # 0.9.10+ - https://chocolatey.org/docs/helpers-uninstall-chocolatey-environment-variable 
 Uninstall-BinFile -Name shipyard -Path "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\shipyard"
-## Remove any shortcuts you added in the install script.
-
